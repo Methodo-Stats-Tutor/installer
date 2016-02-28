d4a4d70be73df7b826a2abd6295aa74a5c83cf32
Nicolas PARIS <nps@ubuntu-laptop>
2016-02-28 18:12:42 +0100

    mst -> mstr

--- roles/frontend/templates/mstrConstant.js
+++ roles/frontend/templates/mstrConstant.js
@@ -1,6 +1,6 @@
-var app = angular.module('mstConstant', []);
+var app = angular.module('mstrConstant', []);
 app.constant('ROOTS', {
-    webServices: 'http://{{ http_host }}:{{ tomcat_port }}/mst',
+    webServices: 'http://{{ http_host }}:{{ tomcat_port }}/mstr',
     ocpu: '//{{ http_host }}:8004/ocpu',
     httpProxy: 'http://{{ http_proxy_host }}:{{ http_proxy_port }}/'
 });
